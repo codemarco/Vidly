a1a992bbb6ed35a9bb3e3044ca8293ac867aad2e
marco.zoppo <marco.zoppo@doubleconsulting.it>
2020-03-19 19:36:37 +0100

    prima dell'esercizio

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+   
+    public class CustomersController : Controller
+    {
+        [Route("Customers/Action")]
+        // GET: Customers
+        public ActionResult ActionCustomers()
+        {
+            var movie = new Movie() {Id = 1, Name = "Shrek"};
+            var viewCustomers = new CustomerViewModel();
+
+            viewCustomers.Customer = new List<Customer>();
+            viewCustomers.Movie = movie;
+
+
+            var customerList = viewCustomers.Customer;
+            
+            customerList.Add(new Customer {Name= "Marco"});
+            customerList.Add(new Customer { Name = "Silvia" });
+
+            
+          
+
+            return View(viewCustomers);
+            
+        }
+    }
+}
